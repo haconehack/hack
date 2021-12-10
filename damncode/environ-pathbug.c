5b9e6e2cf00a1a8eb79b3fc34d4ebd0440de228a
x90 <86798246+haconehack@users.noreply.github.com>
2021-12-11 06:41:47 +0900

    Update and rename bof2-environ.c to environ-pathbug.c

--- damncode/environ-pathbug.c
+++ damncode/environ-pathbug.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 void envvuln_func() {
-    // reportName environment is vulnerable to buffer overflow.
+    // "reportName" is a usercontrolled environment variable. 
     char* rName = getenv("reportName");
     char buf[30];
 
     strncpy(buf, "/home/www/tmp/", 30);
+    // rName(environment) could be used '../../'(related path string) then you can control the unlink path that all you wanted. (bug)
     strncat(buf, rName, 30);
 
     unlink(buf);
